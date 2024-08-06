21001c94f4ff2e3fe55ea2c6c6aa45566db94b21
Yogendra-WWH <yogendra@wealthwisehub.in>
2024-08-06 17:51:58 +0530

    css

--- app.js
+++ app.js
@@ -220,3 +220,30 @@ window.addEventListener("load", function(){
     loader.style.display= "none";
 
 })
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.querySelector(".cta");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function(event) {
+  event.preventDefault(); // Prevent default action of the anchor tag
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
